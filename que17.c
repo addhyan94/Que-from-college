2e8d99b33cac5b961bb0adc3d0fd0abd8c61aa02
Its Tiwari <shubht4732@gmail.com>
2024-09-16 14:04:23 +0530

    Update que17.c

--- que17.c
+++ que17.c
@@ -4,6 +4,13 @@
 int main()
 {
     int l,b,a,p;
-    
+    printf("\n Enter the length = ");
+    scanf("%d",&l);
+    printf("\n Enter the Wridth = ");
+    scanf("%d",&b);;
+    a=l*b;
+    p=2*(l+b);
+    printf("\n Area of Rectangle = %d",a);
+    printf("\n Perimeter of Rectangle = %d",p);
     return 0;
 }
