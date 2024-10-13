5fa4a98131065bad27ea159f911c13a30a5b73a0
Its Tiwari <178887888+addhyan94@users.noreply.github.com>
2024-10-13 15:09:39 +0530

    Add files via upload

--- que106.c
+++ que106.c
@@ -1,13 +1,13 @@
 /* WAP to folloe Series....
-                            2,6,12,24....n terms .*/
+                            3,6,12,24....n terms .*/
 
 #include <stdio.h>
 #include<conio.h>
 
 int main()
  {
-    int i, n,s;
-    s=2;
+    int i, n,s,k;
+    s=3;
     
     printf("\n Enter the number= ");
     scanf("%d", &n);
@@ -15,14 +15,8 @@ int main()
     printf("Series: ");
     for (i = 1; i <= n; i++) 
     {
+        k=s;
         printf("%d, ", s);
-        if (i % 2 > 0) 
-        {
-            s = s * 3; // '2'  ke baad seedhe '6' hai isliye '3' se multiply kiya ..
-        } 
-        else
-         {
-            s = s * 2; 
-        }
+        s=s*2;
     }
 }
