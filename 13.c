474cfd25f042ff2f2d2a91a1fac4b11bfb617dde
addhyan94 <178887888+addhyan94@users.noreply.github.com>
2024-11-01 10:29:03 +0530

    .....

--- 13.c
+++ 13.c
@@ -44,7 +44,7 @@ int main()
   }
 */
 
-#include<stdio.h>
+/*#include<stdio.h>
 #include<conio.h>
  void main()
  {
@@ -55,3 +55,28 @@ int main()
     printf("\n \t mother's name= neetu sahu");
     printf("\n \tdob=09\04\2006 ");
  }
+*/
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int a,b,c,d,e,f,g,h;
+  char ch,name,fname,mname,bname,sname;
+  printf("\n \t Enter first number jo aap ke maan me ho :::   ");
+  scanf("%d",&a);
+  printf("\n\t Enter second number jo aapke maan me ho :::  ");
+  scanf("%d",&b);
+  printf("\n Enter your name :::");
+  scanf("%c",&name);
+printf("\n Enter your Father Name :::  ");
+scanf("%c",&fname);
+printf("\n Enter your Mother Name :::  ");
+scanf("%c",&mname);
+printf("\n Enter your Brother Name :::  ");
+scanf("%c",&bname);
+printf("\n Enter your Sister Name :::  ");
+scanf("%c",&sname);
+
+
+}
--- que112.c
+++ que112.c
@@ -1,65 +1,23 @@
-<<<<<<< HEAD
 /*112....  WAP to print tribinocci serise 
                                 0,1,1,2,4,7,13,24... n terms */
 
-#include <stdio.h>
+#include<stdio.h>
 #include<conio.h>
-
-int main()
- {
-    int n, s, a, b, c, i;
-    a = 0; 
-    b = 1; 
-    c = 1; 
-
-    printf("\n Enter a number = ");
-    scanf("%d", &n); 
-
-    printf("Tribonacci Series: ");
-    if (n >= 1) printf("%d, ", a);
-    if (n >= 2) printf("%d, ", b);
-    if (n >= 3) printf("%d, ", c);
-
-    for (i = 4; i <= n; i++) 
-    {
-        s = a + b + c; 
-        printf("%d, ", s);
-        a = b; 
-        b = c; 
-        c = s; 
-    }
+void main()
+{
+int i,n,a,b,c,s;
+a=-1;
+b=1;
+c=0;
+printf("enter a number=");
+scanf("%d",&n);
+for(i=1; i<=n;i++)
+{
+    s=0;
+    s=a+b+c;
+    a=b;
+    b=c;
+    c=s;
+    printf("%d, ",s);
 }
-
-=======
-/*112....  WAP to print tribinocci serise 
-                                0,1,1,2,4,7,13,24... n terms */
-
-#include <stdio.h>
-#include<conio.h>
-
-int main()
- {
-    int n, s, a, b, c, i;
-    a = 0; 
-    b = 1; 
-    c = 1; 
-
-    printf("\n Enter a number = ");
-    scanf("%d", &n); 
-
-    printf("Tribonacci Series: ");
-    if (n >= 1) printf("%d, ", a);
-    if (n >= 2) printf("%d, ", b);
-    if (n >= 3) printf("%d, ", c);
-
-    for (i = 4; i <= n; i++) 
-    {
-        s = a + b + c; 
-        printf("%d, ", s);
-        a = b; 
-        b = c; 
-        c = s; 
-    }
 }
-
->>>>>>> 2d7c4a7595c9e06094366219d96ea287f5bec18c
