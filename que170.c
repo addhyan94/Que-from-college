c467ebeedbde7fdc1daf1c48c43440374e7c386e
addhyan94 <178887888+addhyan94@users.noreply.github.com>
2024-11-05 00:07:01 +0530

    141 to 170 ~ Patterns

---
+++ que170.c
@@ -0,0 +1,51 @@
+/*  WAP to follow series..... 
+**********
+****  ****
+***    ***
+**      **
+*        *
+*        *
+**      **
+***    ***
+****  ****
+**********
+*/
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int i,j,k,l;
+    for ( i = 5; i>=1 ; i--)
+    {
+        for( j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        for( k=1;k<=(10-2*i);k++)
+        {
+            printf(" ");
+        }
+        for(l=1;l<=i;l++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }   
+    for ( i = 1; i <=5 ; i++)
+    {
+        for( j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        for( k=1;k<=(10-2*i);k++)
+        {
+            printf(" ");
+        }
+        for(l=1;l<=i;l++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }   
+}
