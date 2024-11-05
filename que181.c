f9da836e5f656ac6e15295e41beab741307d00de
addhyan94 <178887888+addhyan94@users.noreply.github.com>
2024-11-05 19:35:19 +0530

    Series using While Loop from 71 to 80

---
+++ que181.c
@@ -0,0 +1,17 @@
+//                                 ~~ Series using While Loop  
+/* WAP to print first five natural number .*/
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int i,a;
+    i = 1;
+    while(  i <=5 )
+    {
+      a=i;
+      printf("\n %d",a);
+    i++;
+    }
+}
