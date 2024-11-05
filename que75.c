9a02d455c3ff370a5a80e07a864f4b597b9124e3
addhyan94 <178887888+addhyan94@users.noreply.github.com>
2024-11-05 20:15:02 +0530

    .....

--- que75.c
+++ que75.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /* WAP to print first five odd number .*/
 
 #include <stdio.h>
@@ -14,21 +14,4 @@ int main()
     }
 
     return 0;
-=======
-/* WAP to print first five odd number .*/
-
-#include <stdio.h>
-#include<conio.h>
-
-int main() 
-{
-    int i;
-   
-    for (i = 1; i <= 9; i =i+ 2) 
-    {
-        printf("%d ", i); 
-    }
-
-    return 0;
->>>>>>> 2d7c4a7595c9e06094366219d96ea287f5bec18c
 }
