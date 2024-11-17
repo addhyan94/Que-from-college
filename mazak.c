5cdffd76ce5f729e48616d55314e3f20b2106ad9
addhyan94 <178887888+addhyan94@users.noreply.github.com>
2024-11-17 12:48:27 +0530

    mazak karna chiye bhai (aapan programmer hai)

--- mazak.c
+++ mazak.c
@@ -1,5 +1,7 @@
+//   Isme aapane 2 program bane hue hai ek hai patterns bala or array bala okee ...
+
 //                                      ~~ Patterns ~~
-#include <stdio.h>
+/*#include <stdio.h>
 #include <conio.h>
 
 int main()
@@ -32,4 +34,35 @@ int main()
         printf("\n");
     }
     
-}
+}*/
+
+// Using array for swapping numbers 
+
+/*#include<stdio.h>
+#include<conio.h>
+int main()
+{
+     int array[5];
+     printf("\n enter a number: ");
+     for (int i = 0; i <=4 ; i++)
+     {
+        scanf("%d",&array[i]);
+     }
+     printf("\n \t Before swapping: \n");
+     for (int i = 0; i <=4; i++)
+     {
+      printf("\n Index = %d , Elements = %d  ",i,array[i]);
+     }
+     int temp;
+     for (int i = 0; i <=2; i++)
+     {
+        temp=array[i];
+       array[i]=array[4-i];
+       array[4-i]=temp;
+     }
+     printf("\n \n\t After swapping : \n");
+      for (int i = 0; i <=4; i++)
+     {
+      printf("\n Index = %d , Elements = %d  ",i,array[i]);
+     }
+}*/
