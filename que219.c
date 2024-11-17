098479ddd108ddc1efe1d853dbdc75d8261e3988
addhyan94 <addhyant85@gmail.com>
2024-11-17 13:19:01 +0530

    using array (211 yo 220)

---
+++ que219.c
@@ -0,0 +1,29 @@
+/*WAP to input 5 elements into an array and swap and swap first element with last element,second element with second last and so on..*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+     int array[5];
+     printf("\n enter a number: ");
+     for (int i = 0; i <=4 ; i++)
+     {
+        scanf("%d",&array[i]);
+     }
+     printf("\n \t Before swapping: \n");
+     for (int i = 0; i <=4; i++)
+     {
+      printf("\n Index = %d , Elements = %d  ",i,array[i]);
+     }
+     int temp;
+     for (int i = 0; i <=2; i++)
+     {
+        temp=array[i];
+       array[i]=array[4-i];
+       array[4-i]=temp;
+     }
+     printf("\n \n\t After swapping : \n");
+      for (int i = 0; i <=4; i++)
+     {
+      printf("\n Index = %d , Elements = %d  ",i,array[i]);
+     }
+}
