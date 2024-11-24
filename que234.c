4b0c3d71b80a263e6a1a7b2666fd5d1305c2fee4
addhyan94 <addhyant85@gmail.com>
2024-11-24 18:37:23 +0530

    2-D Array

--- que234.c
+++ que234.c
@@ -1,2 +1,27 @@
 //                      2-D Array 
 /*WAP to input a matrix of order 3x3 and calculate the sum of elements of both diagonals.*/
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int array[3][3];
+    for (int i = 0; i < 3; i++)
+    {
+    for (int j = 0; j < 3; j++)
+    {
+        printf("INDEX => Row: %d || Colum: %d || Enter Elements :  ",i,j);
+        scanf("%d",&array[i][j]);
+    }
+    }
+    printf("\n Sum of Both Diagonal elements::");
+    int s=0;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            s = s + array[i][j];
+        }
+    }
+    printf("%d",s);
+}
