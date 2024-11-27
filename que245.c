7a4272e27f5819c7a4b9d00f4e38cc57be6a04ae
addhyan94 <addhyant85@gmail.com>
2024-11-27 08:51:35 +0530

    string

---
+++ que245.c
@@ -0,0 +1,43 @@
+//                  String 
+/* WAP to input a string and chack it is palindrine or not Without using strrev function .*/
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char a[50],b[50],ch;
+int i,s,l;
+printf("Enter a number: ");
+gets(a);
+l=strlen(a);
+i=0;
+while (i<l)
+{
+    b[i]=a[l-i-1];
+    i++;
+}
+ b[i] = '\0'; // Null terminate the reversed string hai bhai
+ printf("Your string is : %s : \n",b); // %s ka kaam hai string dena || jo us variable me dala hai ||
+s=strcmp(a,b);
+if (s==0)
+{
+    printf(" palindrine Hai");
+}
+else
+{
+    printf(" palindrine Nahi hai ");
+}
+
+}
+
+/*~~~Output- 1:
+Enter a number: madam
+Your string is : madam : 
+ palindrine Hai
+
+~~~Output- 2:
+Enter a number: Tiwari
+Your string is : irawiT : 
+ palindrine Nahi hai
+
+*/
