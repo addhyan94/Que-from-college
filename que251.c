4a8fd022cae572fb14074f0e99063260a167b1a2
addhyan94 <addhyant85@gmail.com>
2024-12-04 08:30:45 +0530

    Pointers

---
+++ que251.c
@@ -0,0 +1,2 @@
+//                   Pointer
+// Wap to input 2 number and swap these number suing call by reference .
---
+++ que252.c
@@ -0,0 +1,2 @@
+
+// WAP to input length and breadth of a mectangle and calculate its area and perimeter using call by reference .
