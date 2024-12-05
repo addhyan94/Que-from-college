c1d31310703ad1d6ccc79979008b1cf1aeb54885
addhyan94 <addhyant85@gmail.com>
2024-12-05 23:16:31 +0530

    string

--- que248.c
+++ que248.c
@@ -1,37 +1,81 @@
-//                  String 
-/*Wap to input a string and print in the givin Formate.
-Input string Manoj Kumar Verma output string M.K.Verma .    */
+// //                  String 
+// /*Wap to input a string and print in the givin Formate.
+// Input string Manoj Kumar Verma output string M.K.Verma .    */
 #include<stdio.h>
 #include<conio.h>
 #include<string.h>
-//                          sahi ni hai kuki M.K.Verma nahi aa rha ahi M.K.V he aa rha hai 
-void main() 
+void main()
 {
-    char a[50];
-    int i, l;
-    printf("Enter a string: ");
-    gets(a); 
-    l = strlen(a);
-    printf("%c.", a[0]);
-    int k;
-    k=i = 0;
-    while (i < l) 
+    char a[50],ch;
+    int i,l,p;
+    printf("\n enter string=");
+    gets(a);
+    l=strlen(a);
+    printf("%c.",a[0]);
+    for(i=l-1;i>=0;i--)
     {
-        if (a[i] == ' ' && a[i + 1] != '\0') 
+        ch=a[i];
+        if(ch==' ')
         {
-            printf("%c.", a[i + 1]);
-        }
-        i++;
-    }
-    printf(" \n aa gaya j loop ke pass ");
-    for (int j = l - 1; j >= 0; j--) 
-    {
-        printf("\n aa gaya j ke aander ");
-        if (a[j] == ' ') 
-        {
-            printf("%s", &a[j]); 
+            p=i;
             break;
         }
     }
-    printf(" \n aa gaya j loop ke bahar ");
+    for(i=0;i<p;i++)
+    {
+        if(a[i]==' ')
+        {
+            printf("%c.",a[i+1]);
+        }
+    }
+    for(i=p+1;i<l;i++)
+    {
+        printf("%c",a[i]);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<stdio.h>
+// #include<conio.h>
+// #include<string.h>
+// //                          sahi ni hai kuki M.K.Verma nahi aa rha ahi M.K.V he aa rha hai 
+// void main() 
+// {
+//     char a[50];
+//     int i, l;
+//     printf("Enter a string: ");
+//     gets(a); 
+//     l = strlen(a);
+//     printf("%c.", a[0]);
+//     int k;
+//     k=i = 0;
+//     while (i < l) 
+//     {
+//         if (a[i] == ' ' && a[i + 1] != '\0') 
+//         {
+//             printf("%c.", a[i + 1]);
+//         }
+//         i++;
+//     }
+//     printf(" \n aa gaya j loop ke pass ");
+//     for (int j = l - 1; j >= 0; j--) 
+//     {
+//         printf("\n aa gaya j ke aander ");
+//         if (a[j] == ' ') 
+//         {
+//             printf("%s", &a[j]); 
+//             break;
+//         }
+//     }
+//     printf(" \n aa gaya j loop ke bahar ");
+// }
