49d009bbf27aa4099202b7db4d38ed8eeb132f35
addhyan94 <addhyant85@gmail.com>
2024-12-09 23:29:51 +0530

    structure

--- que257.c
+++ que257.c
@@ -2,60 +2,27 @@
 // WAP to input Name , AGE , Percentage of  students and dislay with perper Msg using structure.
 #include<stdio.h>
 #include<conio.h>
-struct marksheet
+struct class
 {
-    int age;
-    float per;
     char name[50];
-} a[5];
+    int age;
+    float pre;
+}a[5];
 void main()
 {
-    for (int i = 1; i < 6; i++)
+    for (int i = 0; i < 5; i++)
     {
-     printf("\n--- Enter Details of Student ( %d ) ---\n", i);
-    printf("\n Student Name : ");
-    scanf("%s",&a[i].name);
-    printf(" Student AGE: ");
-    scanf("%d",&a[i].age);
-    printf(" Student Percentage: ");
-    scanf("%f",&a[i].per);
+        printf("Enter %d Student Name : ",i+1);
+        scanf("%s",&a[i].name);
+        printf("Enter %d Student AGE : ",i+1);
+        scanf("%d",&a[i].age);
+        printf("Enter %d Student Percentage : ",i+1);
+        scanf("%f",&a[i].pre);
     }
-    for (int i = 1; i < 6; i++)
+    for (int i = 0; i < 5; i++)
     {
-    printf("\n--- Details of Student %d ---\n", i);
-        printf("Name: %s\n", a[i].name);
-        printf("Age: %d\n", a[i].age);
-        printf("Percentage: %.2f", a[i].per);
-        printf("\n");
+        printf("\n %d Student Name is = %s",i+1,a[i].name);
+        printf("\n %d Student AGE is = %d",i+1,a[i].age);
+        printf("\n %d Student Percentage is = %.2f",i+1,a[i].pre); 
     }
 }
-
-/*~~~~Output 
---- Enter Details of Student ( 1 ) ---
-
- Student Name : Addhyan
- Student AGE: 18  
- Student Percentage: 65
-
---- Enter Details of Student ( 2 ) ---
-
-
- Student Name : Raju
- Student AGE: 19
- Student Percentage: 98
-
- and so on .................
-
- --- Details of Student 1 ---
-Name: Addhyan
-Age: 18
-Percentage: 65.00
-
---- Details of Student 2 ---
-Name: Raju
-Age: 19
-Percentage: 98.00
-
-and so on .............
-
-*/
