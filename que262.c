17f41ca940d0ec61dd9c7d19bd00732acaaf6471
addhyan94 <addhyant85@gmail.com>
2024-12-11 08:54:00 +0530

    union

--- que262.c
+++ que262.c
@@ -14,16 +14,12 @@ void main()
     {
         printf("Enter %d Student Name: ",i+1);
         scanf("%s",&a[i].name);
+        printf(" Student Name: %s\n",a[i].name);
         printf("Enter %d Student Age: ",i+1);
         scanf("%d",&a[i].age);
+        printf("Age: %d\n", a[i].age);
         printf("Enter %d Student Percentage: ",i+1);
         scanf("%f",&a[i].per);
+        printf("Percentage: %.2f\n", a[i].per);
     }
-     for (int i = 1; i < 6; i++)
-    {
-        printf("%d Student Name: %s\n",i,a[i].name);
-        printf("Age: %d\n", a[i].age);
-        printf("Percentage: %.2f", a[i].per);
-        printf("\n");
-    } 
 }
--- que263.c
+++ que263.c
@@ -1,2 +1,26 @@
 //              union
 // WAP to input a book name , pages number ,and publaction year of 5 book and display all values using union. 
+#include<stdio.h>
+#include<conio.h>
+union Books
+{
+    char name[50];
+    int pages;
+    int year;
+};
+ union Books a[5];
+void main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        printf("\n Enter %d Book Name : ",i+1);
+        scanf("%s",&a[i].name);
+        printf("Book Name Is %s",a[i].name);
+        printf(" Enter %d Books Page Numbers : ",i+1);
+        scanf("%d",&a[i].pages);
+        printf("Pages Number Is %d",a[i].pages);
+        printf(" Enter %d Book Publaction Year : ",i+1);
+        scanf("%d",&a[i].year);
+        printf(" publaction year Is %d",a[i].year);
+    }
+}
