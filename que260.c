eb120710cc86a04e1e0cd952972a087892d65832
addhyan94 <addhyant85@gmail.com>
2024-12-11 09:04:37 +0530

    Structure

--- que260.c
+++ que260.c
@@ -1,2 +1,30 @@
 //                      structure
-// Write a program to input house no. city name and pincode of 5 persons and display these values using structure
+// write a program to input no. of quantity, amount and product name of 5 products and print these record using structure
+#include <stdio.h>
+struct shop
+{
+    char Name[50];
+    int quantity;
+    float amount;
+};
+struct shop product[5];
+void main()
+{
+    int i;
+    for (i = 0; i < 5; i++)
+    {
+        printf("\nEnter the Name of product %d: ", i + 1);
+        scanf(" %s", &product[i].Name);
+        printf("\nEnter the quantity of product %d: ", i + 1);
+        scanf(" %d", &product[i].quantity);
+        printf("\nEnter the amount of product %d: ", i + 1);
+        scanf(" %f", &product[i].amount);
+    }
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("\nThe Name of product %d : %s", i + 1, product[i].Name);
+        printf("\nThe quantity of product %d : %d", i + 1, product[i].quantity);
+        printf("\nThe amount of product %d : %.2f", i + 1, product[i].amount);
+    }
+}
