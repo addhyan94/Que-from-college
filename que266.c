6419911a83ebb9992387a0ab94c4806b1edc459a
Its Tiwari <addhyant85@gmail.com>
2024-12-16 23:18:03 +0530

    File Handling

--- que266.c
+++ que266.c
@@ -3,4 +3,4 @@ Ye bale programs mene ek new folder me dal diye hai uski link me de deta hu neec
 File Handling ke programs hai usme  
 
 
-==>  https://github.com/addhyan94/C-program-codes/tree/tiwari/que266-270%20File%20Handling
+==>  https:github.com/addhyan94/C-program-codes/tree/tiwari/que266-270%20File%20Handling
